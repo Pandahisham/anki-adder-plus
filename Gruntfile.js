9415a3ce01ca443ae00a46b2f4f7bd8c12f0b3c4
Igor Luzhanov <igorl@x-formation.com>
2016-06-27 16:38:49 +0200

    Add sample build files

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+//TODO: fix all paths, this file is for example only
+
+module.exports = function(grunt) {
+    "use strict";
+
+    var contentSrc = ["src/content/*.ts"];
+    var bgSrc = ["src/background/*.ts"];
+    var popupSrc = ["src/popup/*.ts"];
+    var optionSrc = ["src/options/*.ts"];
+
+    grunt.initConfig({
+        ts: {
+            options: {
+                sourceMap: false,
+                removeComments: true
+            },
+            background: {
+                src: bgSrc,
+                out: "build/background/background.js"
+            },
+            popup: {
+                src: popupSrc,
+                out: "build/popup/popup.js"
+            },
+            option: {
+                src: optionSrc,
+                out: "build/options/options.js"
+            },
+            content: {
+                src: contentSrc,
+                out: "build/content/content.js"
+            }
+        },
+        copy: {
+            all: {
+                files: [
+                    {
+                        expand: true,
+                        src: ["manifest.json", "content/*.css", "content/*.js", "pages/*", "popup/popup.html", "options/options.html"],
+                        cwd: "src/",
+                        dest: "build/"
+                    },
+                    {
+                        expand: true,
+                        src: "images/*",
+                        dest: "build/"
+                    }
+                ]
+            }
+        },
+        compress: {
+            main: {
+                options: {
+                    archive: 'regex-search.zip',
+                    mode: 'zip'
+                },
+                files: [
+                    { expand: true, cwd: 'build/', src: '**' }
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-ts");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.registerTask("default", ["ts", "copy"]);
+    grunt.registerTask("release", ["ts", "copy", "compress"]);
+};
